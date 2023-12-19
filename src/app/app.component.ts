311ab4dffd5c857b123f799e6632e3e9f066326f
test <{ID}+{username}@users.noreply.github.com>
2023-12-18 23:56:34 -0600

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterLink, RouterOutlet],
+  template: `
+  <main>
+    <h1>{{defHeader}}</h1>
+  </main>
+  `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+  defHeader = 'ColesHeader';
 }
