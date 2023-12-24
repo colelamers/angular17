71b299a7f7e69085990b6dad3457956c1847cdce
Cole Lamers <colelamers@gmail.com>
2023-12-24 14:50:21 -0600

    angular dev tutorial working up search-functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 import { RouterLink, RouterOutlet } from '@angular/router';
-
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterLink, RouterOutlet],
+  imports: [HomeComponent, RouterLink, RouterOutlet],
   template: `
-  <main>
-    <h1>{{defHeader}}</h1>
-  </main>
+    <main>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+        </header>
+      </a>
+      <section class="content">
+        <router-outlet></router-outlet>
+      </section>
+    </main>
   `,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  defHeader = 'ColesHeader';
+  title = 'homes';
 }
